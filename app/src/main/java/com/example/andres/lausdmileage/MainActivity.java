38dc232fde368caa85d693f60e49b36c1f3cf66d
Andres Rodriguez <andres6459@gmail.com>
2018-08-06 15:50:55 -0700

    Address to Latlngs and road distance calculations working

--- app/src/main/java/com/example/andres/lausdmileage/MainActivity.java
+++ app/src/main/java/com/example/andres/lausdmileage/MainActivity.java
@@ -1,13 +1,140 @@
 package com.example.andres.lausdmileage;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//Convert address to LatLng
+public LatLng getLocationFromAddress(Context context, String strAddress) {
+
+    Geocoder coder = new Geocoder(context);
+    List<Address> address;
+    LatLng p1 = null;
+
+    try {
+        // May throw an IOException
+        address = coder.getFromLocationName(strAddress, 5);
+        if (address == null) {
+            return null;
+        }
+
+        Address location = address.get(0);
+        p1 = new LatLng(location.getLatitude(), location.getLongitude() );
+
+    } catch (IOException ex) {
+
+        ex.printStackTrace();
+    }
+
+    return p1;
+}
+//Calculates road distance of two latitudes and longitudes
+    public String getDistance(final double lat1, final double lon1, final double lat2, final double lon2){
+        final String[] parsedDistance = new String[1];
+        final String[] response = new String[1];
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    URL url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin=" + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2 + "&sensor=false&units=metric&mode=driving");
+                    final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    response[0] = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+
+                    JSONObject jsonObject = new JSONObject(response[0]);
+                    JSONArray array = jsonObject.getJSONArray("routes");
+                    JSONObject routes = array.getJSONObject(0);
+                    JSONArray legs = routes.getJSONArray("legs");
+                    JSONObject steps = legs.getJSONObject(0);
+                    JSONObject distance = steps.getJSONObject("distance");
+                    parsedDistance[0] =distance.getString("text");
+
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return parsedDistance[0];
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+        }
+
+        LatLng myHome = getLocationFromAddress(this, "2629 E Jefferson St");
+
+        Location location = new Location("test");
+        location.setLatitude(myHome.latitude);
+        location.setLongitude(myHome.longitude);
+        location.setTime(new Date().getTime());
+
+        LatLng oldHome = getLocationFromAddress(this, "3124 Josephine St, Lynwood CA");
+
+        Location location2 = new Location("test2");
+        location2.setLatitude(oldHome.latitude);
+        location2.setLongitude(oldHome.longitude);
+        location2.setTime(new Date().getTime());
+
+        String travelDistance = getDistance(location.getLatitude(), location.getLongitude(), location2.getLatitude(), location2.getLongitude());
+
+        Log.i("test", location.toString());
+        Log.i("test-oldHome", oldHome.toString());
+        Log.i("test-travelDistance", travelDistance);
+
     }
 }
+
+
