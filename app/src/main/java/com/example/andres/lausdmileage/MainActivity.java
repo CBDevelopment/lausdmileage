cb4312d1b1866381b4ac9bda1ab40abfcde68af4
Andres Rodriguez <andresrodriguez64@yahoo.com>
2018-08-28 16:31:37 -0700

    Associated school names with addresses

--- app/src/main/java/com/example/andres/lausdmileage/MainActivity.java
+++ app/src/main/java/com/example/andres/lausdmileage/MainActivity.java
@@ -12,11 +12,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
@@ -42,10 +44,18 @@ public class MainActivity extends AppCompatActivity {
     AutoCompleteTextView end_textView;
     Button getDistance;
     TextView textView_showRoadDistance;
+    int startTextViewAddressIndex;
+    int endTextViewAddressIndex;
 
-    private static final String[] ADDRESSES = new String[]{
-            "2629 E Jefferson St, Longbeach", "3124 Josephine St, Lynwood"
+    private static final String[] SCHOOLNAMES = new String[]{
+            "RESEDA HS", "SOCES MAG", "VANALDEN ES", "CALVERT CHRTR FOR ENR STUDIES", "NORTHRIDGE MS", "STAGG ES", "LORNE ES", "ANATOLA ES", "LEMAY ES",
+
     };
+    private static final String[] ADDRESSES = new String[] {
+            "18230 KITTRIDGE ST RESEDA 91335", "18605 ERWIN ST RESEDA 91335", "19019 DELANO ST RESEDA 91335", "19850 DELANO ST WOODLAND HILLS 91367", "17960 CHASE ST NORTHRIDGE 91325",
+            "7839 AMESTOY AVE VAN NUYS 91406", "17440 LORNE ST NORTHRIDGE 91325", "7364 ANATOLA AVE VAN NUYS 91406", "17520 VANOWEN ST VAN NUYS 91406",
+
+};
 
 //Convert address to LatLng
 public LatLng getLocationFromAddress(Context context, String strAddress) {
@@ -82,7 +92,7 @@ public LatLng getLocationFromAddress(Context context, String strAddress) {
                 try {
 
                     URL url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin="
-                            + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2 + "&sensor=false&units=metric&mode=driving");
+                            + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2 + "&sensor=false&units=imperial&mode=driving");
                     final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("POST");
                     InputStream in = new BufferedInputStream(conn.getInputStream());
@@ -142,7 +152,11 @@ public LatLng getLocationFromAddress(Context context, String strAddress) {
 
     public void showDistance(View view) {
 
-            String roadDistance = calculateDistance(start_textView.getText().toString(), end_textView.getText().toString());
+
+
+            String roadDistance = calculateDistance(ADDRESSES[startTextViewAddressIndex], ADDRESSES[endTextViewAddressIndex]);
+
+
 
             textView_showRoadDistance.setText(roadDistance);
 
@@ -154,16 +168,53 @@ public LatLng getLocationFromAddress(Context context, String strAddress) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //possible dropdown solution to display name of school but pass the address to method
-        //Map<String, String> spinnerMap = new HashMap<String, String>();
-
         getDistance = findViewById(R.id.btn_getDistance);
         textView_showRoadDistance = findViewById(R.id.textView_showRoadDistance);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, ADDRESSES);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, SCHOOLNAMES);
         start_textView = findViewById(R.id.start_textView);
         end_textView = findViewById(R.id.end_textView);
         start_textView.setAdapter(arrayAdapter);
         end_textView.setAdapter(arrayAdapter);
+
+        start_textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                int schoolNameIndex;
+
+                for (int x = 0; x < SCHOOLNAMES.length; x++) {
+
+                    if (SCHOOLNAMES[x].equals(start_textView.getText().toString())) {
+
+                        schoolNameIndex = x;
+                        startTextViewAddressIndex = schoolNameIndex;
+                        Toast.makeText(MainActivity.this, ADDRESSES[startTextViewAddressIndex], Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+
+            }
+        });
+
+        end_textView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                int schoolNameIndex;
+
+                for (int x = 0; x < SCHOOLNAMES.length; x++) {
+
+                    if (SCHOOLNAMES[x].equals(end_textView.getText().toString())) {
+
+                        schoolNameIndex = x;
+                        endTextViewAddressIndex = schoolNameIndex;
+                        Toast.makeText(MainActivity.this, ADDRESSES[endTextViewAddressIndex], Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+            }
+        });
 
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -174,5 +225,7 @@ public LatLng getLocationFromAddress(Context context, String strAddress) {
         }
     }
 }
+
+
 
 
